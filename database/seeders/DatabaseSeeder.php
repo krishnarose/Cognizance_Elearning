043b33fe2b965672bce1444aa5f8b51c5a27b7bc
krishnarose <krishnarose2001@gmail.com>
2024-02-28 19:28:59 +0530

    admin logout fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'krishnarose2001@gmail.com',
+            'password' => '12345678',
+            'role' => '1',
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Common User',
+            'email' => 'user@gmail.com',
+            'password' => '12345678',
+            'role' => '0',
+        ]);
     }
 }
--- resources/views/layouts/admin/head.blade.php
+++ resources/views/layouts/admin/head.blade.php
@@ -6,8 +6,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
-
     
+
+
 
 
